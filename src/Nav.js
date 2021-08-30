115dba33f13403f3905e09fe2c177b3bb1a89b3c
PolinaStamenova <polina.stamenova@gmail.com>
2021-08-30 14:32:06 +0300

    Setup empty my profile

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { Link, NavLink, BrowserRouter as Router } from "react-router-dom";
 
 const Nav = () => (
-  <header>
-    <nav></nav>
-  </header>
+  <Router>
+    <header>
+      {/* <NavLink> */}
+      <nav>
+        <ul>
+          {/* <Link path to='/'> */}
+          <li>Rockets</li>
+          {/* </Link> */}
+          {/* <Link path to='/missions'> */}
+          <li>Missions</li>
+          {/* </Link> */}
+          {/* <Link path to='/profile'> */}
+          <li>My profile</li>
+          {/* </Link> */}
+        </ul>
+      </nav>
+      {/* </NavLink> */}
+    </header>
+  </Router>
 );
 
 export default Nav;
