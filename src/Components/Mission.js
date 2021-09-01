31a57112d2c6fa7f765b62ea672d79f50e70c207
PolinaStamenova <polina.stamenova@gmail.com>
2021-09-01 09:25:49 +0300

    Add handleClick func onClick button

--- src/Components/Mission.js
+++ src/Components/Mission.js
@@ -1,20 +1,30 @@
-/* eslint-disable jsx-quotes, quotes */
+/* eslint-disable jsx-quotes, quotes, object-curly-newline */
 
 import React from "react";
+import { useDispatch } from "react-redux";
 import PropTypes from "prop-types";
+import { joinMission } from "../redux/reducers/MissionsReducer";
 
 const Mission = ({ mission }) => {
-  const { id, name, description } = mission;
+  const { id, name, description, join } = mission;
+  const dispatch = useDispatch();
 
+  const handleClick = () => {
+    dispatch(joinMission(id));
+  };
   return (
     <tr key={id}>
       <th>{name}</th>
       <td>{description}</td>
       <td>
-        <button type='button'>NOT A MEMBER</button>
+        <p className='status-placeholder'>
+          {join ? "Active Member" : "NOT A MEMEBER"}
+        </p>
       </td>
       <td>
-        <button type='button'>Join Mission</button>
+        <button type='button' onClick={handleClick}>
+          {join ? "Join Mission" : "Leave mission"}
+        </button>
       </td>
     </tr>
   );
@@ -25,6 +35,8 @@ Mission.propTypes = {
     id: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
+    join: PropTypes.bool.isRequired,
+    status: PropTypes.string.isRequired,
   }).isRequired,
 };
 
