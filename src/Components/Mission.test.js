a25c6dfad0fb777c15a8595d0e929f473634f019
PolinaStamenova <polina.stamenova@gmail.com>
2021-09-02 11:19:56 +0300

    Test

--- src/Components/Mission.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Mission from "./Mission";
-
-it('expect quote page to match working snapshot', () => {
-  const tree = renderer.create(<Mission />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
