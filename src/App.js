8ade487490d77dc3b8ed95ada076a746d4264492
SimonGrchevski <simongrcevski@gmail.com>
2021-09-02 23:00:27 +0200

    Change basename

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import MyProfile from './Components/MyProfile';
 
 const App = () => (
   <div>
-    <Router basename="/Space-Travelers-Hub">
+    <Router basename="/Space-Travelers-Hub/">
       <Nav />
       <Switch>
         <Route exact path="/" component={Rockets} />
